8dbc97cbc63db9e6787072011770f79fb1f79a09
Wojciech Koczy <wojteklony@gmail.com>
2018-06-18 22:11:53 +0200

    Mocks more thesis

--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/controller/ThesisController.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/controller/ThesisController.java
@@ -20,10 +20,11 @@ public class ThesisController {
     @RequestMapping(value = "/api/theses", method = RequestMethod.GET)
     public ResponseEntity<ArrayList<Thesis>> getThesis() {
         theses = new ArrayList<>();
-        theses.add(new Thesis(1, "My topic of work", "MR. Falisz", "http://google.com"));
-        theses.add(new Thesis(2, "Second work topic", "John Nice", "http://google.pl"));
-        theses.add(new Thesis(3, "This is super thesis", "John Nice", "http://google.pl"));
-        theses.add(new Thesis(4, "Whats up guys?", "Ben White", "http://google.pl"));
+        theses.add(new Thesis(1, "How hard is control in single-crossing elections?", "Piotr Faliszewski", "https://link.springer.com/content/pdf/10.1007%2Fs10458-016-9339-3.pdf"));
+        theses.add(new Thesis(2, "Multiwinner Elections With Diversity Constraints", "Piotr Faliszewski", "https://www.aaai.org/ocs/index.php/AAAI/AAAI18/paper/view/16769/15777"));
+        theses.add(new Thesis(3, "Properties of multiwinner voting rules.", "Piotr Faliszewski", "https://link.springer.com/content/pdf/10.1007%2Fs00355-017-1026-z.pdf"));
+        theses.add(new Thesis(4, "Semantic Text Indexing.", "Zbigniew Kaleta", "https://journals.agh.edu.pl/csci/article/view/148/810"));
+        theses.add(new Thesis(5, "Classic and Agent-Based Evolutionary Heuristics for Shape Optimization of Rotating Discs.", "Roman Dębski", "http://www.cai.sk/ojs/index.php/cai/article/view/2017_2_331/823\n"));
 
         return new ResponseEntity<>(theses, HttpStatus.OK);
     }
