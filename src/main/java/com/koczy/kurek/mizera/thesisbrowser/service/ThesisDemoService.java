d581504804c32118f7d740ab4c91990433932fa5
Lony13 <wojteklony@gmail.com>
2018-10-29 20:56:50 +0100

    Update demo service

--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/service/ThesisDemoService.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/service/ThesisDemoService.java
@@ -5,10 +5,7 @@ import com.koczy.kurek.mizera.thesisbrowser.model.ThesisFilters;
 import com.koczy.kurek.mizera.thesisbrowser.model.ThesisResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ThesisDemoService implements IThesisService {
@@ -34,19 +31,24 @@ public class ThesisDemoService implements IThesisService {
     private ArrayList<ThesisDetails> thesesDetails = new ArrayList<ThesisDetails>() {{
         add(new ThesisDetails(0, "How hard is control in single-crossing elections?",
                 "https://link.springer.com/content/pdf/10.1007%2Fs10458-016-9339-3.pdf",
-                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null, null));
+                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null,
+                new HashSet<>(Arrays.asList("test", "demo", "words"))));
         add(new ThesisDetails(1, "Multiwinner Elections With Diversity Constraints",
                 "https://www.aaai.org/ocs/index.php/AAAI/AAAI18/paper/view/16769/15777",
-                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null, null));
+                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null,
+                new HashSet<>(Arrays.asList("test", "demo", "words"))));
         add(new ThesisDetails(2, "Properties of multiwinner voting rules.",
                 "https://link.springer.com/content/pdf/10.1007%2Fs00355-017-1026-z.pdf",
-                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null, null));
+                new ArrayList<>(Collections.singletonList("Piotr Faliszewski")), 5, new Date(), null,
+                new HashSet<>(Arrays.asList("test", "demo", "words"))));
         add(new ThesisDetails(3, "Semantic Text Indexing.",
                 "https://journals.agh.edu.pl/csci/article/view/148/810",
-                new ArrayList<>(Collections.singletonList("Zbigniew Kaleta")), 5, new Date(), null, null));
+                new ArrayList<>(Collections.singletonList("Zbigniew Kaleta")), 5, new Date(), null,
+                new HashSet<>(Arrays.asList("test", "demo", "words"))));
         add(new ThesisDetails(4, "Classic and Agent-Based Evolutionary Heuristics for Shape Optimization of Rotating Discs.",
                 "http://www.cai.sk/ojs/index.php/cai/article/view/2017_2_331/823\n",
-                new ArrayList<>(Collections.singletonList("Roman Dębski")), 5, new Date(), null, null));
+                new ArrayList<>(Collections.singletonList("Roman Dębski")), 5, new Date(), null,
+                new HashSet<>(Arrays.asList("test", "demo", "words"))));
     }};
 
     @Override
