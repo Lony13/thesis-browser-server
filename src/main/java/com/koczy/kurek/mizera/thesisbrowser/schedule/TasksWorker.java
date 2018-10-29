34beaff3a005ab6b16b39ddf078ea3fe2f9174e9
Lony13 <wojteklony@gmail.com>
2018-10-29 18:34:27 +0100

    Fix logger source class

--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/schedule/TasksWorker.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/schedule/TasksWorker.java
@@ -1,6 +1,5 @@
 package com.koczy.kurek.mizera.thesisbrowser.schedule;
 
-import com.koczy.kurek.mizera.thesisbrowser.service.DownloadService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,7 @@ import java.util.logging.Logger;
 @Component
 public class TasksWorker {
 
-    private static final Logger log = Logger.getLogger(DownloadService.class.getName());
+    private static final Logger log = Logger.getLogger(TasksWorker.class.getName());
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
