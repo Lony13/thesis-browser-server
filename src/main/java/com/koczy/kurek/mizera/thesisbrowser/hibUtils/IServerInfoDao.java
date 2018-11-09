21f17dca3ed38e43123bb840240b542e2f973f10
mizeramaciej <botought1>
2018-11-09 21:28:23 +0100

    Implement ServerInfo dao

---
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/hibUtils/IServerInfoDao.java
@@ -0,0 +1,10 @@
+package com.koczy.kurek.mizera.thesisbrowser.hibUtils;
+
+import com.koczy.kurek.mizera.thesisbrowser.model.ServerInfo;
+
+import java.util.List;
+
+public interface IServerInfoDao {
+
+    List<ServerInfo> getServerInfo();
+}
