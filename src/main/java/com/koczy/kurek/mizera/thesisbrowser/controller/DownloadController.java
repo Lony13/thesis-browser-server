4ffee416eaf7dcb8865b5ab13b40ca85d339f1cb
Lony13 <wojteklony@gmail.com>
2018-11-09 19:21:21 +0100

    Make downloadTheses return server info

--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/controller/DownloadController.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/controller/DownloadController.java
@@ -1,5 +1,6 @@
 package com.koczy.kurek.mizera.thesisbrowser.controller;
 
+import com.koczy.kurek.mizera.thesisbrowser.model.ServerInfo;
 import com.koczy.kurek.mizera.thesisbrowser.model.ThesisFilters;
 import com.koczy.kurek.mizera.thesisbrowser.service.IDownloadService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,7 @@ public class DownloadController {
     }
 
     @RequestMapping(value = "/theses/download", method = RequestMethod.POST)
-    public ResponseEntity downloadTheses(@RequestBody ThesisFilters thesisFilters) {
+    public ResponseEntity<ServerInfo> downloadTheses(@RequestBody ThesisFilters thesisFilters) {
         return downloadService.downloadTheses(thesisFilters);
     }
 
