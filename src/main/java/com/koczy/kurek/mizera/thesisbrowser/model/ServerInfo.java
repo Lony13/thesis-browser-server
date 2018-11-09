73bc5365dbf6f16eaf1725d9aaad6ff7992164d3
Lony13 <wojteklony@gmail.com>
2018-11-09 19:17:38 +0100

    Return server info form lda method

--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/model/ServerInfo.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/model/ServerInfo.java
@@ -1,4 +1,30 @@
 package com.koczy.kurek.mizera.thesisbrowser.model;
 
+import java.util.Date;
+
 public class ServerInfo {
+
+    private Date date;
+    private String message;
+
+    public ServerInfo(Date date, String message) {
+        this.date = date;
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/com/koczy/kurek/mizera/thesisbrowser/service/ThesisService.java
+++ src/main/java/com/koczy/kurek/mizera/thesisbrowser/service/ThesisService.java
@@ -8,7 +8,6 @@ import com.koczy.kurek.mizera.thesisbrowser.model.ThesisResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
